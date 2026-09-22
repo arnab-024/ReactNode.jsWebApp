11e8df9f00f8fab3c3db077a9bce05bd8283ebe6
arnab-024 <arnsin04@gmail.com>
2026-09-22 23:26:01 +0530

    frontend https changes added

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -1,8 +1,22 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+
+  server: {
+    host: "localhost",
+    port: 5173,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "certs/localhost-key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost.pem")),
+    },
+  },
+});
